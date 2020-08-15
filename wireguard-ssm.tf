bab53dbd4092efdcf6aed0b01a24581366a3a499
Heorhi Sakovich <heorhi.sakovich@gmail.com>
2020-08-15 22:24:48 +0300

    create ssm param instead of discovering exsisting

--- wireguard-ssm.tf
+++ wireguard-ssm.tf
@@ -1,3 +1,5 @@
-data "aws_ssm_parameter" "wg_server_private_key" {
+resource "aws_ssm_parameter" "wg_server_private_key" {
   name = var.wg_server_private_key_param
+  type = "SecureString"
+  value = var.wg_server_private_key_value
 }
